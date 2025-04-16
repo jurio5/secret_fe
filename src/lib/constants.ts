5f6035572ab90670a789cf485e903f6ba8a7b8a6
jurio5 <sia5492@naver.com>
2025-04-16 09:11:17 +0900

    [feat] 프로젝트 전역 상수 아바타 추가

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+/**
+ * 프로젝트 전체에서 사용되는 상수 정의
+ */
+
+// 기본 아바타 URL
+export const DEFAULT_AVATAR = 'https://quizzle-avatars.s3.ap-northeast-2.amazonaws.com/%EA%B8%B0%EB%B3%B8+%EC%95%84%EB%B0%94%ED%83%80.png'; 
