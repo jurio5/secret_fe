e3caa4c327c2d61147518712f088500afde0bc43
jurio5 <sia5492@naver.com>
2025-04-16 09:09:14 +0900

    [feat] 전역 기본 프로필 아바타 등록

--- src/lib/utils/apiUtils.ts
+++ src/lib/utils/apiUtils.ts
@@ -66,4 +66,7 @@ export const fetchWithAuthErrorHandling = async <T>(
     
     throw error;
   }
-}; 
+};
+
+// 기본 아바타 URL - 전역 상수로 사용
+export const DEFAULT_AVATAR = 'https://quizzle-avatars.s3.ap-northeast-2.amazonaws.com/%EA%B8%B0%EB%B3%B8+%EC%95%84%EB%B0%94%ED%83%80.png'; 
